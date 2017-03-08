879371704238ff1f1983e34d81415b01bcf1a9d1
Dominic Lynch <dom_lynch@yahoo.com>
2017-03-08 09:18:22 +0000

    renamed variables and used them all

--- CoderDojo/src/com/coderdojo/inverness/tutorials/ArrayTutorial.java
+++ CoderDojo/src/com/coderdojo/inverness/tutorials/ArrayTutorial.java
@@ -4,16 +4,12 @@ public class ArrayTutorial {
 
 	public static void main(String[] args) {
 		
-		int a = 1;
-		int b = 5;
-		int c = 173621735;
+		int[] integers1 = new int[3];
+		integers1[0] = 15;
+		integers1[1] = 30;
+		integers1[2] = 45;
 		
-		int[] array1 = new int[3];
-		array1[0] = 15;
-		array1[1] = 30;
-		array1[2] = 45;
-		
-		int[] array2 = {11, 22, 33, 44, 55};
+		int[] integers2 = {11, 22, 33, 44, 55};
 		
 		boolean[] bools = {true, false, true, false};
 		float[] floats = {1.2f, 3.14159f, 6.67f};
@@ -22,12 +18,22 @@ public class ArrayTutorial {
 		names[1] = "Vasily";
 		names[2] = "Brian";
 		
-		for (int index = 0; index < array1.length; index++)
+		for (int index = 0; index < integers1.length; index++)
 		{
-			System.out.println(array1[index]);
+			System.out.println(integers1[index]);
 		}
 		
-		for (int val : array2)
+		for (int val : integers2)
+		{
+			System.out.println(val);
+		}
+		
+		for (boolean val : bools)
+		{
+			System.out.println(val);
+		}
+		
+		for (float val : floats)
 		{
 			System.out.println(val);
 		}
